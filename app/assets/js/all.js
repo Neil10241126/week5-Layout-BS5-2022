ca61444e05e47c0de007f217bc1c687c726afb7e
Neil <Neil10241126@gmail.com>
2022-08-25 20:51:40 +0800

    新增分頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,24 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(function(){
+  // 下拉式選單
+  $('.dropdown').click(function(e){
+    event.preventDefault();
+
+    $(this).toggleClass('active');
+    $(this).siblings().slideToggle();
+  });
+
+  // Reply 留言板呼叫
+  $('.reply-btn').click(function(e){
+    event.preventDefault();
+
+    $('.hide, .chevron').addClass('none');
+    $('.reply-box').addClass('change');
+  });
+
+  $('.back-btn').click(function(e){
+    event.preventDefault();
+
+    $('.reply-box').removeClass('change');
+    $('.hide, .chevron').removeClass('none');
+  });
 });
