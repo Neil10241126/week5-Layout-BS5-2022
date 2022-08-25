6bbce9d769bb0b46389925f9f82f7d2fcb7f23da
Neil <Neil10241126@gmail.com>
2022-08-25 20:50:47 +0800

    Update 2022-08-25T12:50:42.500Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,22 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  // 下拉式選單
+  $('.dropdown').click(function (e) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $(this).siblings().slideToggle();
+  }); // Reply 留言板呼叫
+
+  $('.reply-btn').click(function (e) {
+    event.preventDefault();
+    $('.hide, .chevron').addClass('none');
+    $('.reply-box').addClass('change');
+  });
+  $('.back-btn').click(function (e) {
+    event.preventDefault();
+    $('.reply-box').removeClass('change');
+    $('.hide, .chevron').removeClass('none');
+  });
 });
 //# sourceMappingURL=all.js.map
